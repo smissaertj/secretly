5ea220aba0948020c873a02770d9365b30252faf
Joeri Smissaert <smissaertj@gmail.com>
2022-08-30 20:13:51 +0400

    Create DB entry, Hash/Verify password, Send email, return JSON responses

---
+++ app/helpers.py
@@ -0,0 +1,44 @@
+import sendgrid
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+from decouple import config
+from sendgrid.helpers.mail import Mail, Email, To, Content
+
+# Password verification
+def hash_password(password):
+    argon2Hasher = PasswordHasher(
+        time_cost=3, # number of iterations
+        memory_cost=64 * 1024, # 64mb
+        parallelism=1, # how many parallel threads to use
+        hash_len=32, # the size of the derived key
+        salt_len=16 # the size of the random generated salt in bytes
+    )
+    hash = argon2Hasher.hash(password)
+    return hash
+
+
+def verify_password(hash, passwd):
+    try:
+        ph = PasswordHasher()
+        return ph.verify(hash, passwd)
+    except VerifyMismatchError:
+        return False
+
+# Email sending
+def send_mail(to_email, uuid):
+    try:
+        sg = sendgrid.SendGridAPIClient(api_key=config('SENDGRID_API_KEY'))
+        from_email = Email(config('SENDGRID_FROM_EMAIL'))
+        to_email = To(to_email)
+        subject = "You received an encrypted message!"
+        content = Content('text/html', uuid)
+        mail = Mail(from_email, to_email, subject, content)
+
+        mail_json = mail.get()
+        response = sg.client.mail.send.post(request_body=mail_json)
+        if response.status_code == 202 or response.status_code == 200:
+            return True
+        else:
+            return False
+    except Exception:
+        return False
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,128 @@
-from app import app
+from app import app, helpers, models
+from flask import request, make_response, jsonify
 
-@app.route('/')
-def index():
-    return "Hello World!!!!!!"
+@app.route('/api/new_message', methods=['POST'])
+def new_message():
+    try:
+        content_type = request.headers.get('Content-Type')
+        if content_type == 'application/json':
+
+            # Parse the JSON from the POST request
+            data = request.get_json()[0]
+            message = data['message']
+            password = data['password']
+            to_email = data['to_email']
+
+            # Hash the password
+            passwd_hash = helpers.hash_password(password)
+
+            # Create new Message instance and save data to database
+            new_message = models.Message(message_txt=message, passwd_hash=passwd_hash)
+            new_message.addToDB()
+
+            # Send email with message.uuid
+            sg_response = helpers.send_mail(to_email, new_message.uuid)
+            if sg_response:
+                response = make_response(
+                    jsonify(
+                        {
+                            'response_msg': 'Email sent!',
+                            'severity': 'success',
+                            'msg_uuid': new_message.uuid
+                         }
+                    ),
+                    200
+                )
+                return response
+            else:
+                response = make_response(
+                    jsonify(
+                        {
+                            'response_msg': 'Error sending email. You may want to deliver the UUID yourself :(',
+                            'severity': 'error',
+                            'msg_uuid': new_message.uuid
+                        }
+                    ),
+                    500
+                )
+                return response
+        else:
+            response = make_response(
+                jsonify(
+                    {
+                        'response_msg': 'Content-Type should be application/json',
+                        'severity': 'error'
+                     },
+                ),
+                400
+            )
+            return response
+    except KeyError:
+        response = make_response(
+            jsonify(
+                {
+                    'response_msg': 'Required data missing in POST request.',
+                    'severity': 'error'
+                }
+            ),
+            400
+        )
+        return response
+
+
+@app.route('/api/read_message', methods=['POST'])
+def read_message():
+    try:
+        content_type = request.headers.get('Content-Type')
+        if content_type == 'application/json':
+            data = request.get_json()[0]
+            uuid = data['uuid']
+            password = data['password']
+
+            retrieved_msg = models.Message.query.filter_by(uuid=uuid).first()
+
+            # Return the message if it exists and the password matches the hashed password
+            if retrieved_msg != None and helpers.verify_password(retrieved_msg.passwd_hash, password):
+                response = make_response(
+                    jsonify(
+                        {
+                            'response_msg': f'{retrieved_msg.message}',
+                            'severity': 'success'
+                         },
+                    ),
+                    401
+                )
+                return response
+            else:
+                response = make_response(
+                    jsonify(
+                        {
+                            'response_msg': 'Invalid message UUID or wrong password.',
+                            'severity': 'error'
+                         },
+                    ),
+                    401
+                )
+                return response
+        else:
+            response = make_response(
+                jsonify(
+                    {
+                        'response_msg': 'Content-Type should be application/json',
+                        'severity': 'error'
+                     },
+                ),
+                400
+            )
+            return response
+    except KeyError:
+        response = make_response(
+            jsonify(
+                {
+                    'response_msg': 'Required data missing in POST request.',
+                    'severity': 'error'
+                }
+            ),
+            400
+        )
+        return response
