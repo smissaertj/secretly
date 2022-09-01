ee11200f46b178561d9f6174cc20f871c501ea35
Joeri Smissaert <smissaertj@gmail.com>
2022-09-01 12:10:39 +0400

    Create One To Many relationship between User and Message

--- secretly-api/app/models.py
+++ secretly-api/app/models.py
@@ -2,6 +2,33 @@ from app import db
 from datetime import datetime
 import os
 
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+    passwd_hash = db.Column(db.String(128))
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    create_date = db.Column(db.Date)
+    is_admin = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean, default=True)
+    messages = db.relationship('Message', backref='user')
+
+    def __init__(self, email, passwd_hash):
+        self.email = email
+        self.passwd_hash = passwd_hash
+        self.create_date = datetime.now().strftime('%Y-%m-%d')
+
+    def addToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'<id {self.id}>'
+
+
 class Message(db.Model):
     __tablename__ = 'messages'
 
@@ -12,6 +39,7 @@ class Message(db.Model):
     create_date = db.Column(db.Date)
     read_date = db.Column(db.Date)
     is_read = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, message_txt, passwd_hash):
         self.uuid = os.urandom(24).hex()
@@ -27,26 +55,3 @@ class Message(db.Model):
         return f'<id {self.id}>'
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String)
-    passwd_hash = db.Column(db.String(128))
-    first_name = db.Column(db.String())
-    last_name = db.Column(db.String())
-    create_date = db.Column(db.Date)
-    is_admin = db.Column(db.Boolean, default=False)
-    is_active = db.Column(db.Boolean, default=True)
-
-    def __init__(self, email, passwd_hash):
-        self.email = email
-        self.passwd_hash = passwd_hash
-        self.create_date = datetime.now().strftime('%Y-%m-%d')
-
-    def addToDB(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def __repr__(self):
-        return f'<id {self.id}>'
