079a20361465bf3e6b108ba750748cd1f5e3c98c
Joeri Smissaert <smissaertj@gmail.com>
2022-09-01 12:21:45 +0400

    Updated one to many relationship

--- secretly-api/app/models.py
+++ secretly-api/app/models.py
@@ -7,14 +7,14 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
     passwd_hash = db.Column(db.String(128))
     first_name = db.Column(db.String())
     last_name = db.Column(db.String())
     create_date = db.Column(db.Date)
     is_admin = db.Column(db.Boolean, default=False)
     is_active = db.Column(db.Boolean, default=True)
-    messages = db.relationship('Message', backref='user')
+    messages = db.relationship('Message', backref='users')
 
     def __init__(self, email, passwd_hash):
         self.email = email
@@ -39,7 +39,7 @@ class Message(db.Model):
     create_date = db.Column(db.Date)
     read_date = db.Column(db.Date)
     is_read = db.Column(db.Boolean, default=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __init__(self, message_txt, passwd_hash):
         self.uuid = os.urandom(24).hex()
