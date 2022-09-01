70959698052ba7d6a62a97af8c6388040b6010cb
Joeri Smissaert <smissaertj@gmail.com>
2022-09-01 10:11:46 +0400

    JWT Auth handling

--- secretly-api/app/models.py
+++ secretly-api/app/models.py
@@ -1,7 +1,6 @@
 from app import db
 from datetime import datetime
 import os
-# from sqlalchemy.dialects.postgresql import JSON
 
 class Message(db.Model):
     __tablename__ = 'messages'
@@ -17,6 +16,31 @@ class Message(db.Model):
     def __init__(self, message_txt, passwd_hash):
         self.uuid = os.urandom(24).hex()
         self.message = message_txt
+        self.passwd_hash = passwd_hash
+        self.create_date = datetime.now().strftime('%Y-%m-%d')
+
+    def addToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'<id {self.id}>'
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+    passwd_hash = db.Column(db.String(128))
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    create_date = db.Column(db.Date)
+    is_admin = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean, default=True)
+
+    def __init__(self, email, passwd_hash):
+        self.email = email
         self.passwd_hash = passwd_hash
         self.create_date = datetime.now().strftime('%Y-%m-%d')
 
--- secretly-api/app/routes.py
+++ secretly-api/app/routes.py
@@ -1,7 +1,90 @@
 import datetime
-
+import jwt
 from app import app, db, helpers, models
-from flask import request, make_response, jsonify
+from flask import request
+from functools import wraps
+
+def token_required(f):
+    """ Decorator function to check if the Authorization header is passed in the request, return a 403 if missing."""
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].split(' ')[1]
+
+        if not token:
+            return helpers.json_response('Authentication Token Missing!', 'error', 401)
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            current_user = models.User.filter_by(id=data['user_id'])
+
+            if current_user == None:
+                return helpers.json_response('Invalid Authentication Token!', 'error', 401)
+
+            if not current_user['is_active']:
+                return helpers.json_response('Account is deactivated!', 'error', 403)
+
+        except Exception as e:
+            return helpers.json_response(str(e), 'error', 500)
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
+# TODO - Signup/Register
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    try:
+        content_type = request.headers.get('Content-Type')
+        if content_type == 'application/json':
+
+            # Parse the JSON from the POST request
+            data = request.get_json()
+            email = data['email']
+            password = data['password']
+
+            passwd_hash = helpers.hash_password(password)
+
+            new_user = models.User(email=email, passwd_hash=passwd_hash)
+            new_user.addToDB()
+
+            return helpers.json_response('Thank you. You can now login', 'success', 200)
+
+    except KeyError:
+        return helpers.json_response('Required data missing in POST request.', 'error', 400)
+
+# TODO - Email Activation
+
+
+# TODO - Login
+@app.route('/api/login', methods=['POST'])
+def login():
+    try:
+        content_type = request.headers.get('Content-Type')
+        if content_type == 'application/json':
+
+            # Parse the JSON from the POST request
+            data = request.get_json()
+            email = data['email']
+            password = data['password']
+
+            user = models.User.query.filter_by(email=email).first()
+
+            if user:
+                if helpers.verify_password(user.passwd_hash, password):
+                    token = jwt.encode({
+                        'public_id': user.id,
+                        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)
+                    }, app.config['SECRET_KEY'], "HS256")
+
+                    return helpers.json_response('Login successful!', 'success', 200, token=token)
+
+            return helpers.json_response('Invalid email or password!', 'error', 401)
+    except KeyError:
+        return helpers.json_response('Required data missing in POST request.', 'error', 400)
+
 
 @app.route('/api/new_message', methods=['POST'])
 def new_message():
@@ -52,12 +135,12 @@ def read_message():
                     retrieved_msg.is_read != True and \
                     helpers.verify_password(retrieved_msg.passwd_hash, password):
                 # Mark the message as read
-                retrieved_msg.is_read = True
+                # retrieved_msg.is_read = True
                 retrieved_msg.read_date = datetime.datetime.now()
                 db.session.add(retrieved_msg)
                 db.session.commit()
 
-                return helpers.json_response(retrieved_msg.message, 'success', 200)
+                return helpers.json_response(retrieved_msg.message, 'success', 200, retrieved_msg.uuid)
 
             else:
                 return helpers.json_response('Invalid message UUID or wrong password.', 'error', 401)
